30f41aeb37e2aaae2917694a773f6074769af47b
Akshata <70626299+akshata2000-23@users.noreply.github.com>
2022-10-07 12:18:38 +0530

    Update main.tf
    Added s3 bucket creation

--- main.tf
+++ main.tf
@@ -20,3 +20,12 @@ instance_type = "t2.micro"
      Name = "Akshata-ec2-final"
    }
  }
+#creating s3 bucket
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+    tags = {
+     Name = "Akshata-s3"
+   }
+  
+}
